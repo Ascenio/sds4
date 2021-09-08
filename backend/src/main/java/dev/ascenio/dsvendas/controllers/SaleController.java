2d6d9944bca5b64d8ae29891618c731c1b6e875c
Ascênio Sanderson <ascenio@hotmail.com>
2021-09-08 13:59:54 -0300

    build(backend): ready for production

--- backend/src/main/java/dev/ascenio/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/dev/ascenio/dsvendas/controllers/SaleController.java
@@ -3,11 +3,14 @@ package dev.ascenio.dsvendas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.ascenio.dsvendas.dto.SaleDTO;
 import dev.ascenio.dsvendas.dto.SalesSuccessDTO;
 import dev.ascenio.dsvendas.dto.SalesSumDTO;
 import dev.ascenio.dsvendas.services.SaleService;
@@ -17,6 +20,12 @@ import dev.ascenio.dsvendas.services.SaleService;
 public class SaleController {
 	@Autowired
 	private SaleService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+		var sales = service.findAll(pageable);
+		return ResponseEntity.ok(sales);
+	}
 	
 	@GetMapping("/ammount-by-seller")
 	public ResponseEntity<List<SalesSumDTO>> ammountBySeller() {
@@ -28,5 +37,5 @@ public class SaleController {
 	public ResponseEntity<List<SalesSuccessDTO>> successBySeller() {
 		var reports = service.successBySeller();
 		return ResponseEntity.ok(reports);
-	}//
+	}
 }
