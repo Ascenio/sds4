537c3e9dc68e3f2fa051e50e79e0590deb6f6256
Ascênio Sanderson <ascenio@hotmail.com>
2021-09-10 14:19:35 -0300

    feat(frontend): gets sales' sums from backend

--- frontend/src/components/donutchart/index.tsx
+++ frontend/src/components/donutchart/index.tsx
@@ -1,23 +1,50 @@
-import Chart from 'react-apexcharts';
+import axios from "axios";
+import Chart from "react-apexcharts";
+import { SaleSum } from "types/sale";
+import { BASE_URL } from "utils/requests";
+
+type ChartData = {
+  series: number[];
+  labels: string[];
+};
 
 export default function DonutChart() {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
-    const options = {
-        legend: {
-            show: true
-        }
-    }
-    
-    return (
-        <Chart
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
-            type='donut'
-            height="240"
-        />
+  let chartData: ChartData = {
+    series: [],
+    labels: [],
+  };
+
+  axios.get(`${BASE_URL}/sales/ammount-by-seller`).then((response) => {
+    const data = response.data as SaleSum[];
+    chartData = data.reduce<ChartData>(
+      (chartData, sale) => {
+        const { series, labels } = chartData;
+        return {
+          series: [...series, sale.sum],
+          labels: [...labels, sale.sellerName],
+        };
+      },
+      { series: [], labels: [] }
     );
+  });
+
+  //const mockData = {
+  //    series: [477138, 499928, 444867, 220426, 473088],
+  //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  //}
+
+  const options = {
+    legend: {
+      show: true,
+    },
+  };
+
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+    />
+  );
 }
