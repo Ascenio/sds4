3bdbbea2c3e2ff2326785235aadf941a64b70554
Ascênio Sanderson <ascenio@hotmail.com>
2021-09-10 13:59:57 -0300

    feat(frontend): adds navigation

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,11 +1,14 @@
 import Logo from 'assets/img/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
         <div className="container">
           <nav className="my-2 my-md-0 mr-md-3">
-            <img src={Logo} alt="DevSuperior" width="120" />
+            <Link to="/">
+              <img src={Logo} alt="DevSuperior" width="120" />
+            </Link>
           </nav>
         </div>
       </div>
